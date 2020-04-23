dc5bde0cf9b039ee9d9199afe08f514ec2aeda48
dicky.akbar@dwidasa.com <akbar128256512>
2020-04-23 14:26:40 +0700

    commit

--- controller/login.php
+++ controller/login.php
@@ -6,23 +6,21 @@
 
     $sql = "SELECT * FROM m_user WHERE nip = '$nip' AND password = '$password'";
     $login = pg_query($sql);
+    $row = pg_fetch_row($login);
     $cek = pg_numrows($login);
 
-    if($cek > 0){
+    if ($cek > 0) {
         session_start();
         $_SESSION['username'] = $nip;
-        $_SESSION['name'] = $login[1];
+        $_SESSION['name'] = $row[1];
         $_SESSION['status'] = "login";
 
-        echo '<script language="javascript">';
-        echo 'alert("' . $login . '")';
-        echo '</script>';
-
         header("location:../pages/dashboard.php");
-    }else{
+    } else {
         echo '<script language="javascript">';
         echo 'alert("NIP dan Password tidak valid")';
         echo '</script>';
+
         header("location:../index.php");
     }
 ?>
