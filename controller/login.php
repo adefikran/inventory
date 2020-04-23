7246616380805b541180f4d11d9a260b2c41b9c1
dicky.akbar@dwidasa.com <akbar128256512>
2020-04-23 12:53:34 +0700

    commit

--- controller/login.php
+++ controller/login.php
@@ -1,10 +1,10 @@
 <?php
-    include 'connection.php';
-
     $nip = $_POST['nip'];
     $password = $_POST['password'];
 
-    $login = pg_query("SELECT * FROM m_user WHERE nip = 'nip' AND password = '$password'");
+    $db = pg_connect("host=ec2-52-6-143-153.compute-1.amazonaws.com port=5432 dbname=dfmdrkhqrs7bcb user=qduzbatmrhpxym password=7da9ed9d8e99236811f4f90337240dcc9c5f6bad6a622b9e4606f911b67f4b02");
+    $sql = "SELECT * FROM m_user WHERE nip = 'nip' AND password = '$password'";
+    $login = pg_exec($db, $sql);
     $cek = pg_numrows($login);
 
     if($cek > 0){
