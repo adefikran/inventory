7fae3814237f71051524ac19b9ef5b8cd13a09b7
dicky.akbar@dwidasa.com <akbar128256512>
2020-04-26 14:39:40 +0700

    commit

--- controller/transaction.php
+++ controller/transaction.php
@@ -12,13 +12,11 @@
             $row = pg_fetch_row($transaction);
 
             for ($count = 0; $count < count($_POST["item_category"]); $count++) {
-                $data = array(
-                    ':item_category'    => $_POST["item_category"],
-                    ':item_quantity'    => $_POST["item_quantity"],
-                    ':item_note'        => $_POST["item_note"]
-                );
+                $itemCategory = $_POST["item_category"];
+                $itemQuantity = $_POST["item_quantity"];
+                $itemNote = $_POST["item_note"];
 
-                $query = "INSERT INTO t_transaction_detail (transaction_id, barang_id, quantity, note) VALUES ($row[0], :item_category, :item_quantity, :item_note)";
+                $query = "INSERT INTO t_transaction_detail (transaction_id, barang_id, quantity, note) VALUES ($row[0], $itemCategory, $itemQuantity, '$itemNote')";
                 $result = pg_query($query);
             }
 
