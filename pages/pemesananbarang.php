f787a028c6fb3eaae0beaa2e1a908e13dd551686
dicky.akbar@dwidasa.com <akbar128256512>
2020-04-26 10:32:25 +0700

    commit

--- pages/pemesananbarang.php
+++ pages/pemesananbarang.php
@@ -40,6 +40,29 @@
     }
 ?>
 
+<script type="text/javascript">
+    function add() {
+        alert("SHOW");
+    }
+
+    $(document).ready(function () {
+        $(document).on('click', '.add', function () {
+            var html = '';
+            html += '<tr>';
+            html += '<td><select name="item_name[]" class="form-control item_name"><option value="">Pilih Barang</option><?php echo fillBarang(); ?></select></td>';
+            html += '<td><input type="text" name="item_quantity[]" class="form-control item_quantity" /></td>';
+            html += '<td><input type="button" name="remove" class="btn btn-danger btn-sm remove"><span class="glyphicon glyphicon-minus"></span></input></td>';
+            html += '</tr>';
+
+            $('#item_table').append(html);
+        });
+
+        $(document).on('click', '.remove', function () {
+            $(this).closest('tr').remove();
+        });
+    });
+</script>
+
 <div class="wrapper">
     <header class="main-header">
         <a href="dashboard.php" class="logo">
@@ -104,7 +127,7 @@
                             <tr>
                                 <th>Nama Barang</th>
                                 <th>Quantity</th>
-                                <th><button type="button" name="add" class="btn btn-success btn-sm add" onclick="add"><span class="glyphicon glyphicon-plus"></span></button> </th>
+                                <th><button type="button" name="add" class="btn btn-success btn-sm add" onclick="add();"><span class="glyphicon glyphicon-plus"></span></button> </th>
                             </tr>
                         </table>
                         <br />
@@ -126,29 +149,6 @@
     </aside>
     <div class="control-sidebar-bg"></div>
 </div>
-
-<script type="text/javascript">
-    function add() {
-        alert("SHOW");
-    }
-
-    $(document).ready(function () {
-        $(document).on('click', '.add', function () {
-            var html = '';
-            html += '<tr>';
-            html += '<td><select name="item_name[]" class="form-control item_name"><option value="">Pilih Barang</option><?php echo fillBarang(); ?></select></td>';
-            html += '<td><input type="text" name="item_quantity[]" class="form-control item_quantity" /></td>';
-            html += '<td><button type="button" name="remove" class="btn btn-danger btn-sm remove"><span class="glyphicon glyphicon-minus"></span></button></td>';
-            html += '</tr>';
-
-            $('#item_table').append(html);
-        });
-
-        $(document).on('click', '.remove', function () {
-            $(this).closest('tr').remove();
-        });
-    });
-</script>
 
 <script src="../style/css/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="../style/css/bower_components/jquery-ui/jquery-ui.min.js"></script>
