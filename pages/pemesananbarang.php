1a58f0dfd530dee9fb9abf67e21af127b3cf6403
dicky.akbar@dwidasa.com <akbar128256512>
2020-04-26 11:02:53 +0700

    commit

--- pages/pemesananbarang.php
+++ pages/pemesananbarang.php
@@ -19,6 +19,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script async='async' src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
@@ -99,18 +100,19 @@
         <section class="content">
             <div class="form-group">
                 <form method="post" id="insert_form">
-                    <div class="table-responsive">
+                    <div class="table-repsonsive">
+                        <span id="error"></span>
                         <table class="table table-bordered" id="item_table">
                             <thead>
-                                <tr>
-                                    <th>Nama Barang</th>
-                                    <th>Quantity</th>
-                                    <th><button type="button" name="add" class="btn btn-success btn-xs add"><span class="glyphicon glyphicon-plus"></span></button> </th>
-                                </tr>
+                            <tr>
+                                <th>Enter Item Name</th>
+                                <th>Category</th>
+                                <th>Sub Category</th>
+                                <th><button type="button" name="add" class="btn btn-success btn-xs add"><span class="glyphicon glyphicon-plus"></span></button></th>
+                            </tr>
                             </thead>
                             <tbody></tbody>
                         </table>
-                        <br />
                         <div align="center">
                             <input type="submit" name="submit" class="btn btn-info" value="Insert" />
                         </div>
@@ -129,28 +131,6 @@
     </aside>
     <div class="control-sidebar-bg"></div>
 </div>
-
-<script>
-    $(document).ready(function () {
-        var count = 0;
-
-        $(document).on('click', '.add', function () {
-            count++;
-            var html = '';
-            html += '<tr>';
-            html += '<td><select name="item_name[]" class="form-control item_name" data-sub_category_id="'+count+'"><option value="">Pilih Barang</option><?php echo fillBarang(); ?></select></td>';
-            html += '<td><input type="text" name="item_quantity[]" class="form-control item_quantity" /></td>';
-            html += '<td><button type="button" name="remove" class="btn btn-danger btn-sm remove"><span class="glyphicon glyphicon-minus"></span></button></td>';
-            html += '</tr>';
-
-            $('tbody').append(html);
-        });
-
-        $(document).on('click', '.remove', function () {
-            $(this).closest('tr').remove();
-        });
-    });
-</script>
 
 <script src="../style/css/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="../style/css/bower_components/jquery-ui/jquery-ui.min.js"></script>
@@ -175,3 +155,106 @@
 <script src="../style/css/dist/js/demo.js"></script>
 </body>
 </html>
+<script>
+    $(document).ready(function(){
+
+        var count = 0;
+
+        $(document).on('click', '.add', function(){
+            count++;
+            var html = '';
+            html += '<tr>';
+            html += '<td><input type="text" name="item_name[]" class="form-control item_name" /></td>';
+            html += '<td><select name="item_category[]" class="form-control item_category" data-sub_category_id="'+count+'"><option value="">Select Category</option><option value="2">Chemicals</option><option value="5">Electronics</option><option value="15">Food</option><option value="16">Textile</option></select></td>';
+            html += '<td><select name="item_sub_category[]" class="form-control item_sub_category" id="item_sub_category'+count+'"><option value="">Select Sub Category</option></select></td>';
+            html += '<td><button type="button" name="remove" class="btn btn-danger btn-xs remove"><span class="glyphicon glyphicon-minus"></span></button></td>';
+            $('tbody').append(html);
+        });
+
+        $(document).on('click', '.remove', function(){
+            $(this).closest('tr').remove();
+        });
+
+        $(document).on('change', '.item_category', function(){
+            var category_id = $(this).val();
+            var sub_category_id = $(this).data('sub_category_id');
+            $.ajax({
+                url:"fill_sub_category.php",
+                method:"POST",
+                data:{category_id:category_id},
+                success:function(data)
+                {
+                    var html = '<option value="">Select Sub Category</option>';
+                    html += data;
+                    $('#item_sub_category'+sub_category_id).html(html);
+                }
+            })
+        });
+
+        $('#insert_form').on('submit', function(event){
+            event.preventDefault();
+            var error = '';
+            $('.item_name').each(function(){
+                var count = 1;
+                if($(this).val() == '')
+                {
+                    error += '<p>Enter Item name at '+count+' Row</p>';
+                    return false;
+                }
+                count = count + 1;
+            });
+
+            $('.item_category').each(function(){
+                var count = 1;
+
+                if($(this).val() == '')
+                {
+                    error += '<p>Select Item Category at '+count+' row</p>';
+                    return false;
+                }
+
+                count = count + 1;
+
+            });
+
+            $('.item_sub_category').each(function(){
+
+                var count = 1;
+
+                if($(this).val() == '')
+                {
+                    error += '<p>Select Item Sub category '+count+' Row</p> ';
+                    return false;
+                }
+
+                count = count + 1;
+
+            });
+
+            var form_data = $(this).serialize();
+
+            if(error == '')
+            {
+                $.ajax({
+                    url:"insert.php",
+                    method:"POST",
+                    data:form_data,
+                    success:function(data)
+                    {
+                        if(data == 'ok')
+                        {
+                            $('#item_table').find('tr:gt(0)').remove();
+                            $('#error').html('<div class="alert alert-success">Item Details Saved</div>');
+                        }
+                    }
+                });
+            }
+            else
+            {
+                $('#error').html('<div class="alert alert-danger">'+error+'</div>');
+            }
+
+        });
+
+    });
+</script>
