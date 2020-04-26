2d3b08b7ebf0cdbc5d3503af64fa020a15f61810
dicky.akbar@dwidasa.com <akbar128256512>
2020-04-26 11:06:57 +0700

    commit

--- pages/pemesananbarang.php
+++ pages/pemesananbarang.php
@@ -165,7 +165,7 @@
             var html = '';
             html += '<tr>';
             html += '<td><input type="text" name="item_name[]" class="form-control item_name" /></td>';
-            html += '<td><select name="item_category[]" class="form-control item_category" data-sub_category_id="'+count+'"><option value="">Select Category</option></select></td>';
+            html += '<td><select name="item_category[]" class="form-control item_category" data-sub_category_id="'+count+'"><option value="">Select Category</option><?php echo fillBarang();?></select></td>';
             html += '<td><select name="item_sub_category[]" class="form-control item_sub_category" id="item_sub_category'+count+'"><option value="">Select Sub Category</option></select></td>';
             html += '<td><button type="button" name="remove" class="btn btn-danger btn-xs remove"><span class="glyphicon glyphicon-minus"></span></button></td>';
             $('tbody').append(html);
